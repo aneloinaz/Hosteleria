693f1a2b428433e34981e7014853fa2df6822a71
Mendez <ikyemendez24@lhusurbil.eus>
2025-05-12 04:51:17 -0400

    Estructura del menu de comandero

---
+++ Comandero/menu.js
@@ -0,0 +1,24 @@
+// al cargar la pàgina debe de solicitar todas las categorias
+// la categoria que estè en primer lugar debe de traer sus subcategorias por defecto
+document.addEventListener('DOMContentLoaded',()=>{
+    //Se deben de detectar que categoria es seleccionada y sacar su id
+    //Se manda ese id a un fetch con GET y se traen las subcategorias que pertenezcan a ese id
+    //Si se hace click en una subcategoria, se manda el id de esa subcategoria y se trae todos los productos relacionados a ese id de subcategoria
+
+    //debes de retornar un array segun lo que se necesite ya sea productos, lista de categorias o subcategorias
+
+});
+
+// funciones que haràn fetch de tipo GET
+function fetchCategorias(){
+
+}
+
+function fetchSubCategorias(idCategoria){
+
+}
+
+function fetchProductos(idSubCategoria){
+
+}
+
