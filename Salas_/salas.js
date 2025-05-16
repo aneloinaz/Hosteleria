df6bd0f9e5b0d0ac67f06879bb5a013c694f7db2
Ibai <ikimatarranz24@lhusurbil.eus>
2025-05-16 14:09:57 +0200

    js de mesas y validacion del boton de numero de comensales

--- Salas_/salas.js
+++ Salas_/salas.js
@@ -1,11 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
   const mesas = document.querySelectorAll('.mesa');
-
   mesas.forEach(mesa => {
     mesa.addEventListener('click', function() {
       const idMesa = this.getAttribute('data-id');
       localStorage.setItem('mesaSeleccionada', idMesa);
-      window.location.href = 'mesasHTML/mesa1.html';
+     window.location.href = '../mesasHTML/mesa1.html';
     });
   });
 });
--- mesasHTML/mesa1.js
+++ mesasHTML/mesa1.js
@@ -1,16 +1,24 @@
 const idMesa = localStorage.getItem('mesaSeleccionada');
+const numeroSpan = document.querySelector('.numero');
+const entradaInput = document.querySelector('.entrada');
+const botonAceptar = document.querySelector('.boton-aceptar');
 
-if(idMesa){
-    /*llmada (idMesa); */
-} else {
-    console.error('No se ha seleccionado ninguna mesa');
-    alert('No se ha seleccionado ninguna mesa');
-} 
-
-function /* llamada(idMesa) */{
-    const apiUrl = 'https://apiostalaritza.lhusurbil.eus/';
+/* aqui va la llamada a la api para que devuelva la capacidad de la mesa segun su idMesa*/
 
 
+botonAceptar.addEventListener('click', function() {
+  const cantidadIngresada = parseInt(entradaInput.value);
+  if (isNaN(cantidadIngresada) || cantidadIngresada <= 0) {
+    alert('Por favor, ingresa un número válido de comensales.');
+    return;
+  }
 
-
-}
+  // Validar que no supere la capacidad
+  const capacidadMax = parseInt(numeroSpan.textContent);
+  if (cantidadIngresada > capacidadMax) {
+    alert(`La cantidad ingresada supera la capacidad máxima de la mesa (${capacidadMax}).`);
+    return;
+  }
+  localStorage.setItem('comensalesMesa', cantidadIngresada);
+  window.location.href = '../Comandero/menu.html';
+});
