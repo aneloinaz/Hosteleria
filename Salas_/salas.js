fa6dcfa32dfd24028b538b538e6b09360e91a5a0
Ibai <ikimatarranz24@lhusurbil.eus>
2025-05-16 10:26:25 +0200

    js de salas  añadido

--- Salas_/salas.js
+++ Salas_/salas.js
@@ -1,1 +1,11 @@
-const hola = "hhola";
+document.addEventListener('DOMContentLoaded', function() {
+  const mesas = document.querySelectorAll('.mesa');
+
+  mesas.forEach(mesa => {
+    mesa.addEventListener('click', function() {
+      const idMesa = this.getAttribute('data-id');
+      localStorage.setItem('mesaSeleccionada', idMesa);
+      window.location.href = 'mesasHTML/mesa1.html';
+    });
+  });
+});
---
+++ mesasHTML/mesa1.js
@@ -0,0 +1,16 @@
+const idMesa = localStorage.getItem('mesaSeleccionada');
+
+if(idMesa){
+    /*llmada (idMesa); */
+} else {
+    console.error('No se ha seleccionado ninguna mesa');
+    alert('No se ha seleccionado ninguna mesa');
+} 
+
+function /* llamada(idMesa) */{
+    const apiUrl = 'https://apiostalaritza.lhusurbil.eus/';
+
+
+
+
+}
