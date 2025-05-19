2411add741e7771f137a3658f7cd69f3e5e10c50
Ibai <ikimatarranz24@lhusurbil.eus>
2025-05-19 13:04:38 +0200

    mesasEstado.js actualizado

--- Salas_/mesasEstado.js
+++ Salas_/mesasEstado.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', async () => {
-    const mesas = document.getElementsByClassName("mesa"); // Corregido: sin punto
+    const mesas = document.getElementsByClassName("mesa"); 
+
     Array.from(mesas).forEach(async item => {
         const ID = item.dataset.id;
         await BuscarEstadoMesa(ID, item);
@@ -8,20 +9,24 @@ document.addEventListener('DOMContentLoaded', async () => {
 
 async function fetchMesasEstado(ID){
     try{
-        const response = await fetch(`https://apiostalaritza.lhusurbil.eus/GetComandasMesaAbiertas?idMesa=${ID}`);
+        const response = await fetch(`https://apiostalaritza.lhusurbil.eus/GetEstadoMesa?idMesa=${ID}`);
         const data = await response.json();
         return data;
     }catch(e){
         console.error("Error al hacer la peticion al servidor : "+e);
     }
 }
-
+    
 async function BuscarEstadoMesa(ID, mesaElemento){
     const data = await fetchMesasEstado(ID);
     if (!data || !('comandas' in data)) return;
-    if(data.comandas.length <= 0){ 
+    if(estado == 0){ 
         mesaElemento.style.backgroundColor = "green";
-    }else{
+    }else if(estado == 1){
         mesaElemento.style.backgroundColor = "red";
+    }else if(estado == 2){
+        mesaElemento.style.backgroundColor = "blue"
+    }else if(estado == 3){
+        mesaElemento.style.backgroundColor = "yellow"
     }
 }
