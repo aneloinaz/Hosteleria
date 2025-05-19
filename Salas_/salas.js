da2c8f7e613a4eba6c5d28ecd9674744f716ea8c
Mendez <ikyemendez24@lhusurbil.eus>
2025-05-19 02:48:38 -0400

    Responsive de salas

--- Salas_/salas.js
+++ Salas_/salas.js
@@ -1,1 +1,25 @@
-const hola = "hhola";
+// Mendez
+document.addEventListener('DOMContentLoaded', async () => {
+    const salasData = await getSalas();
+    let index = 0;
+    if (window.location.pathname.endsWith('sala2.html')) {
+        index = 1;
+    }
+    generarMesas(salasData.salas[index].mesas);
+});
+
+const generarMesas = (mesas) => {
+    const container = document.getElementById("plano");
+    const mesasHTML = mesas.map(item =>
+        `<button class="mesa n${item.id}" data-id="${item.id}">${item.id}</button>`
+    ).join('');
+    container.innerHTML = mesasHTML;
+}
+
+async function getSalas() {
+    const response = await fetch("salas.json");
+    const data = await response.json();
+    return data;
+}
+
+
