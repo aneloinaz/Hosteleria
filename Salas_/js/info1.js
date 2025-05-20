b676b3870387deb466ab0bf2cf6918324254a948
Mendez <ikyemendez24@lhusurbil.eus>
2025-05-20 05:51:56 -0400

    prueba

--- Salas_/js/info1.js
+++ Salas_/js/info1.js
@@ -13,41 +13,41 @@ if (idMesa){
   alert('No se selecciono ninguna mesa');
 }
 
-function consultarCapacidad(idMesa) {
-  // const url = `https://apiostalaritza.lhusurbil.eus/GetMesa?idMesa=${idMesa}/capacidad`;
-  const url = "./json/salas.json";
-  fetch(url)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Error de red: ' + response.status);
-      }
-      return response.json();
-    })
-    .then(data => {
-      //Se modifica para que se ajuste al json de prueba
-      // Buscar la mesa por id en todas las salas
-      let capacidad = null;
-      for (const sala of data.salas) {
-        const mesa = sala.mesas.find(m => m.id == idMesa);
-        if (mesa) {
-          capacidad = mesa.capacidad;
-          break;
-        }
-      }
-      if (capacidad !== null) {
-        console.log(`Capacidad de la mesa ${idMesa}:`, capacidad);
-        numeroSpan.textContent = capacidad;
-      } else {
-        console.error('No se encontró la mesa con id', idMesa);
-        alert('No se encontró la mesa seleccionada.');
-      }
-    })
-    .catch(error=>{
-      console.error('Error al consultar la capacidad de la mesa ;', error);
-      alert('Error al consultar la capacidad de la mesa. Por favor, vuelve a intentarlo mas tarde.')
+// function consultarCapacidad(idMesa) {
+//   // const url = `https://apiostalaritza.lhusurbil.eus/GetMesa?idMesa=${idMesa}/capacidad`;
+//   const url = "./json/salas.json";
+//   fetch(url)
+//     .then(response => {
+//       if (!response.ok) {
+//         throw new Error('Error de red: ' + response.status);
+//       }
+//       return response.json();
+//     })
+//     .then(data => {
+//       //Se modifica para que se ajuste al json de prueba
+//       // Buscar la mesa por id en todas las salas
+//       let capacidad = null;
+//       for (const sala of data.salas) {
+//         const mesa = sala.mesas.find(m => m.id == idMesa);
+//         if (mesa) {
+//           capacidad = mesa.capacidad;
+//           break;
+//         }
+//       }
+//       if (capacidad !== null) {
+//         console.log(`Capacidad de la mesa ${idMesa}:`, capacidad);
+//         numeroSpan.textContent = capacidad;
+//       } else {
+//         console.error('No se encontró la mesa con id', idMesa);
+//         alert('No se encontró la mesa seleccionada.');
+//       }
+//     })
+//     .catch(error=>{
+//       console.error('Error al consultar la capacidad de la mesa ;', error);
+//       alert('Error al consultar la capacidad de la mesa. Por favor, vuelve a intentarlo mas tarde.')
 
-    })
-  }
+//     })
+//   }
 
 
 
--- Salas_/js/salas.js
+++ Salas_/js/salas.js
@@ -1,6 +1,7 @@
 import { actualizarEstado } from "./mesasEstado.js";
 
 document.addEventListener('DOMContentLoaded', async () => {
+    localStorage.clear();
     let cont  = 0;
     async function actualizarMesas() {
         const salasData = await getSalas();
