f65fdefb8e2ff20f61d51e79527a491a612b2329
Ibai <ikimatarranz24@lhusurbil.eus>
2025-05-21 12:50:09 +0200

    solo se puede entrar en comanda si la mesa esta en rojo o en naranja

--- Salas_/js/info1.js
+++ Salas_/js/info1.js
@@ -1,9 +1,9 @@
-const idMesa = localStorage.getItem('mesaSeleccionada');
+/*const idMesa = localStorage.getItem('mesaSeleccionada');
 const numeroSpan = document.querySelector('.numero');
 const entradaInput = document.querySelector('.entrada');
 const botonAceptar = document.querySelector('.boton-aceptar');
 
-/* aqui va la llamada a la api para que devuelva la capacidad de la mesa segun su idMesa*/
+ aqui va la llamada a la api para que devuelva la capacidad de la mesa segun su idMesa
 
 
 // if (idMesa){
@@ -58,14 +58,15 @@ botonAceptar.addEventListener('click', function() {
     alert('Por favor, ingresa un número válido de comensales.');
     return;
   }
-/*
+
   // Validar que no supere la capacidad
   const capacidadMax = parseInt(numeroSpan.textContent);
   if (cantidadIngresada > capacidadMax) {
     alert(`La cantidad ingresada supera la capacidad máxima de la mesa (${capacidadMax}).`);
     return;
   }
-     */
+     
   localStorage.setItem('comensalesMesa', cantidadIngresada);
   window.location.href = '../Comandero/html/menu.html';
 });
+*/
