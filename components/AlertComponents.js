f799e49784d38668b0b989ce73f241634e5c68fa
Mendez <ikyemendez24@lhusurbil.eus>
2025-05-21 08:18:31 -0400

    AlertPromt

--- components/AlertComponents.js
+++ components/AlertComponents.js
@@ -61,3 +61,51 @@ export function AlertConfirm(message,redirection = null){
         container.appendChild(div);
     });
 }
+
+export function AlertPrompt(message){
+        return new Promise((resolve)=>{
+        const container = document.querySelector("body");
+        container.style.overflow = "hidden";
+        // div
+        const div = document.createElement('div');
+        div.classList.add("alertComponent","alertMessage");
+        const p = document.createElement('p');
+        p.classList.add("alertMessageText");
+        p.textContent = message;
+        //input pidiendo datos
+        const input = document.createElement('input');
+        
+
+
+
+
+        
+        //btn aceptar
+        const btnAceptar = document.createElement('span');
+        btnAceptar.textContent = "ACEPTAR";
+        btnAceptar.classList.add("alertMessageBtnAceptar","alertBtn");
+        btnAceptar.addEventListener('click',()=>{
+            div.remove();
+            resolve(true);
+            if(redirection){
+                window.location.href = redirection;
+            };
+        });
+        //btn cancelar
+        const btnCancelar = document.createElement('span');
+        btnAceptar.textContent = "CANCELAR";
+        btnAceptar.classList.add("alertMessageBtnCancelar","alertBtn");
+        btnCancelar.addEventListener('click',()=>{
+            div.remove();
+            resolve(false);
+            if(redirection){
+                window.location.href = redirection;
+            };
+        });
+    
+        div.appendChild(p);
+        div.appendChild(btnAceptar);
+        div.appendChild(btnCancelar);
+        container.appendChild(div);
+    });
+}
