8648b2458ec49b26af32b4bfbf2e7bbe9755c916
ane loinaz <ikaloinaz24@lhusurbil.eus>
2025-05-26 08:05:22 +0200

    arreglando errores

--- Reservas/Salas_/js/mesasEstado.js
+++ Reservas/Salas_/js/mesasEstado.js
@@ -1,64 +1,65 @@
-export async function actualizarEstado(){
-      //const fecha = new Date().toISOString().slice(0,10);
-      const fecha=localStorage.getItem("fecha");
+export async function actualizarEstado() {
+  //const fecha = new Date().toISOString().slice(0,10);
+  const fecha = localStorage.getItem("fecha");
   const mesas = document.getElementsByClassName("mesa");
 
-    const estados = await cargarEstadosDesdeJSON(fecha);
-    actualizarContador(fecha);
+  const estados = await cargarEstadosDesdeJSON(fecha);
+  actualizarContador(fecha);
 
-    Array.from(mesas).forEach(mesa => {
-        const ID = parseInt(mesa.dataset.id);
-        const mesaEstado = estados.estados.find(m => m.idMesa === ID);
-        if (mesaEstado) {
-            cambiarColorMesa(mesa, mesaEstado.estado);
-        }
-    });
+  Array.from(mesas).forEach(mesa => {
+    const ID = parseInt(mesa.dataset.id);
+    const mesaEstado = estados.estados.find(m => m.idMesa === ID);
+    if (mesaEstado) {
+      cambiarColorMesa(mesa, mesaEstado.estado);
+    }
+  });
 }
 
- async function cargarEstadosDesdeJSON(fecha) {
-    try {
-        const response = await fetch(`https://apiostalaritza.lhusurbil.eus/GetEstadoMesas?fecha=${fecha}`);  // const response = await fetch('https://apiostalaritza.lhusurbil.eus/GetmesasEsado?idMesa=${idMesa}'); 
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error('Error al cargar el archivo:', error);
-        return [];
-    }
+async function cargarEstadosDesdeJSON(fecha) {
+  try {
+    const response = await fetch(`https://apiostalaritza.lhusurbil.eus/GetEstadoMesas?fecha=${fecha}`);  // const response = await fetch('https://apiostalaritza.lhusurbil.eus/GetmesasEsado?idMesa=${idMesa}'); 
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error al cargar el archivo:', error);
+    return [];
+  }
 }
 
 function cambiarColorMesa(mesaElemento, estado) {
-   if (estado==0) {
-     mesaElemento.style.backgroundColor = "green"; // Libre
-     mesaElemento.addEventListener('click', function() {
-                const idMesa = this.getAttribute('data-id'); 
-                localStorage.setItem('mesaSeleccionada', idMesa);
-                window.location.href = '/Reservas/html/iconfirmacion.html'});
-   } else if (estado==1 || estado==2 || estado==3) {
-     mesaElemento.style.backgroundColor = "red"; // Reservado
-     /*mesaElemento.addEventListener('click', function() {
-                const idMesa = this.getAttribute('data-id'); 
-                localStorage.setItem('mesaSeleccionada', idMesa);
-                 window.location.href = '/Reservas/html/iconfirmacion.html'});*/
-   };
+  if (estado == 0) {
+    mesaElemento.style.backgroundColor = "green"; // Libre
+    mesaElemento.addEventListener('click', function () {
+      const idMesa = this.getAttribute('data-id');
+      localStorage.setItem('mesaSeleccionada', idMesa);
+      window.location.href = '/Reservas/html/iconfirmacion.html'
+    });
+  } else if (estado == 1 || estado == 2 || estado == 3) {
+    mesaElemento.style.backgroundColor = "red"; // Reservado
+    /*mesaElemento.addEventListener('click', function() {
+               const idMesa = this.getAttribute('data-id'); 
+               localStorage.setItem('mesaSeleccionada', idMesa);
+                window.location.href = '/Reservas/html/iconfirmacion.html'});*/
+  };
 }
 
-async function actualizarContador(fecha){
+async function actualizarContador(fecha) {
   const data = await getComensalesMesa(fecha);
   let comensales = 0;
-  data.mesas.forEach(mesa =>{
-    comensales=+ mesa.numComensales;
-    console.log("Num Mesa: "+mesa.numMesa + "comensales: " + mesa.numComensales);
+  data.mesas.forEach(mesa => {
+    comensales = + mesa.numComensales;
+    console.log("Num Mesa: " + mesa.numMesa + "comensales: " + mesa.numComensales);
   });
-  
+
   document.getElementById("contador").textContent = comensales;
 }
 
-async function getComensalesMesa(fecha){
-  try{
+async function getComensalesMesa(fecha) {
+  try {
     const response = await fetch(`https://apiostalaritza.lhusurbil.eus/GetMesasOcupadasFecha?fecha=${fecha}`);
     const data = await response.json();
     return data;
-  }catch(e){
-    console.error("Error al hacer la peticion: "+e);
+  } catch (e) {
+    console.error("Error al hacer la peticion: " + e);
   }
 }
