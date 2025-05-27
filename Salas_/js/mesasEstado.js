5a716fe418dc80fd4d6706e8262b5c62f45c6a6e
Mendez <ikyemendez24@lhusurbil.eus>
2025-05-27 03:46:14 -0400

    comentarios

--- Salas_/js/mesasEstado.js
+++ Salas_/js/mesasEstado.js
@@ -1,19 +1,25 @@
+//funcion que actualiza el estado de las mesas
 export async function actualizarEstado(){
-      const fecha = new Date().toISOString().slice(0,10);
+  //Se toma la fecha actual
+  const fecha = new Date().toISOString().slice(0,10);
+  //Se llama al contenedor que almacena las mesas
   const mesas = document.getElementsByClassName("mesa");
-
+  //Se llama a la funcion que devuelve los estados segun la fecha
     const estados = await cargarEstadosDesdeJSON(fecha);
+    //Se llama a la funcion que actualiza el contador, se le pasa la fecha como referencia
     actualizarContador(fecha);
-
+    //Convierte la coleccion de HTML de mesas a un array iterable
+    //de cada mesa saco su data-id para poder buscarlo en los estados de las mesas y asi poder actualizarlo
     Array.from(mesas).forEach(mesa => {
         const ID = parseInt(mesa.dataset.id);
         const mesaEstado = estados.estados.find(m => m.idMesa === ID);
         if (mesaEstado) {
+            //funcion que cambia el estado, recibe el nodo y el estado, 
             cambiarColorMesa(mesa, mesaEstado.estado);
         }
     });
 }
-
+//funcion que hace la peticion get de los estados de las mesas segun la fecha
  async function cargarEstadosDesdeJSON(fecha) {
     try {
         const response = await fetch(`https://apiostalaritza.lhusurbil.eus/GetEstadoMesas?fecha=${fecha}`);  // const response = await fetch('https://apiostalaritza.lhusurbil.eus/GetmesasEsado?idMesa=${idMesa}'); 
@@ -24,28 +30,32 @@ export async function actualizarEstado(){
         return [];
     }
 }
-
+//funcion que cambia el color de las mesas
 function cambiarColorMesa(mesaElemento, estado) {
+  //Si el estado es 0 es libre y se pinta en verde
    if (estado==0) {
      mesaElemento.style.backgroundColor = "green"; // Libre
 
    } else if (estado==1) {
+    //Si es 1 es reservado y se pinta en rojo
      mesaElemento.style.backgroundColor = "red"; // Reservado
      mesaElemento.addEventListener('click', function() {
                 const idMesa = this.getAttribute('data-id'); 
                 localStorage.setItem('mesaSeleccionada', idMesa);
                  window.location.href = '../Comandero/html/menu.html'});
    } else if (estado==2) {
+    //Si es 2 hay comanda abierta y se pinta en naranja
      mesaElemento.style.backgroundColor = "orange"; // Comanda pedida
      mesaElemento.addEventListener('click', function() {
                 const idMesa = this.getAttribute('data-id'); 
                 localStorage.setItem('mesaSeleccionada', idMesa);
                  window.location.href = '../Comandero/html/menu.html'});
    } else if (estado==3) {
+    //Si el estado es 3 , la mesa ya ha facturado
      mesaElemento.style.backgroundColor = "blue"; // Finalizado
 }
 }
-
+// funcion que actualiza el contador de clientes
 async function actualizarContador(fecha){
   const data = await getComensalesMesa(fecha);
   let comensales = 0;
@@ -55,7 +65,7 @@ async function actualizarContador(fecha){
   });
   document.getElementById("contador").textContent = comensales;
 }
-
+//Se consulta el estado de las mesas y se saca sus comensales
 async function getComensalesMesa(fecha){
   try{
     const response = await fetch(`https://apiostalaritza.lhusurbil.eus/GetMesasOcupadasFecha?fecha=${fecha}`);
--- Salas_/js/salas.js
+++ Salas_/js/salas.js
@@ -1,49 +1,66 @@
 import { actualizarEstado } from "./mesasEstado.js";
+//Se importan los scripts del estado de las mesas
 
+//Al cargar el DOM se ejecuta:
 document.addEventListener('DOMContentLoaded', async () => {    
+    //Se elimina el LocalStorage
     localStorage.clear();
+    //inicializo el contador de clientes
     let cont  = 0;
+    //funcion que consulta salas y genera mesas
     async function actualizarMesas() {
+        //hace el llamado a la funcion que realiza el GET a salas
         const salasData = await getSalas();
+        //guardo las salas
+        //inicializo un contador para poder gestionar que sala se va a mostrar, segun el HTML
         let index = 0;
+        //Si la ruta termina en sala2.html entonces index vale 1 sino 0 
         if (window.location.pathname.endsWith('sala2.html')) {
             index = 1;
         }
+        //Se hace la llamada a la funcion que trae las mesas
+        //Se le pasa el id de sala
         const mesasData = await getMesas(salasData.salas[index].idSala);
+        //Con las mesas ya listas, se llama a la funcion que genera las mesas en el DOM
         generarMesas(mesasData);
-
+        //Luego de generarlas se actualiza su estado mediante la funcion actualizarEstado()
         await actualizarEstado();
         console.log("actualizacion: "+(cont++));
     }
 
-    // Llamada inicial
+    // Se llama a la funcion para que muestre las mesas
     await actualizarMesas();
-    // Actualización cada 5 segundos
+    // Actualiza las mesas ya creadas cada 5 segundos
     setInterval(actualizarEstado, 5000);
 });
 
+//funcion que genera mesa
 const generarMesas = (mesas) => {
+    //Se llama al contenedor
     const container = document.getElementById("plano");
+    //Se itera con un map en el array de mesas que ha recibido
+    //Se genera el HTML de button que representa cada mesa
+    //Se le pasan los valores dinamicamente
+    //Se le pasa el ID mediante un atributo personalizado
     const mesasHTML = mesas.mesas.map(item =>
         `<button class="mesa n${item.numMesa}" data-id="${item.numMesa}">${item.numMesa}</button>`
     ).join('');
+    //Se inserta en el contenedor
     container.innerHTML = mesasHTML;
 }
-
+//funcion que hace la peticion GET a salas
 async function getSalas() {
     const response = await fetch("https://apiostalaritza.lhusurbil.eus/GetSalas");
     const data = await response.json();
     return data;
 }
-
+//funcion que hace la peticion GET a mesas
 async function getMesas(idSala){
     const response = await fetch(`https://apiostalaritza.lhusurbil.eus/GetMesas?idSala=${idSala}`);
     const data = await response.json();
     return data;
 }
-
-
-
+//Se llama al button para que te lleve a reserva
 const btnRes = document.getElementById('reservar');
 btnRes.addEventListener('click', function() {
     window.location.href = '../Reservas/html/indexsalas.html';
